f841b7bc207099f83c229ed5cd500ca381a69325
Arham Sarwar <raheemdevsinn@gmail.com>
2026-08-11 18:06:25 +0500

    add clarity code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,16 @@ gtag('js', new Date());
 gtag('config', 'G-4WD5FEECW3');`,
           }}
         ></script>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "y0pl00mkld");`,
+          }}
+        ></script>
       </head>
       <body className="min-h-screen flex flex-col m-0 p-0 overflow-x-hidden bg-background text-foreground">
         <Navbar />
